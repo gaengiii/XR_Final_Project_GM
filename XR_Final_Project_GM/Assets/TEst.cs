b04d1ee0db98cc96d61a848aa60057aed6af4dfb
pencils57 <gyeongmi314@gmail.com>
2023-05-30 18:15:07 +0900

    정수형 변수 추가
    int a, b, c추가했음

---
+++ XR_Final_Project_GM/Assets/TEst.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TEst : MonoBehaviour
+{
+    int a, b, c;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
